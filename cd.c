04a561c23f01b003263b7dcbb69da962a57759e4
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-23 18:21:02 +0200

    Create cd.c

---
+++ cd.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+ * change_directory - Implements the cd built-in command
+ * @path: The path to change to
+ *
+ * Task 10: cd
+ *
+ * Description: This function changes the current directory of the process.
+ */
+void change_directory(char *path)
+{
+    /* Code to implement cd */
+}
