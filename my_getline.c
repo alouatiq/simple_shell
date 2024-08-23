cb69e1838c9187a67839e6bf8d3e1caf0f5df573
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-23 18:03:16 +0200

    Create my_getline.c

---
+++ my_getline.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * my_getline - Custom implementation of getline function
+ * @lineptr: Pointer to the buffer containing the line
+ * @n: Pointer to the size of the buffer
+ * @stream: Input stream to read from
+ *
+ * Task 6: Simple Shell 0.1.1
+ *
+ * Return: Number of characters read, -1 on failure.
+ */
+ssize_t my_getline(char **lineptr, size_t *n, FILE *stream)
+{
+    /* Code to implement the getline function */
+}
