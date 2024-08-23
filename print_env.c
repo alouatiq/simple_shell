948cefa5c7a396f989465a5488e7f05e6fa58321
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-23 18:03:05 +0200

    Create print_env.c

---
+++ print_env.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * print_env - Prints the current environment variables
+ *
+ * Task 5: Simple Shell 1.0
+ *
+ * Description: This function prints all the environment variables.
+ */
+void print_env(void)
+{
+    /* Code to print all environment variables */
+}
