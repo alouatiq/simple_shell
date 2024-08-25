02f5596b2c13b4399e670fae23b0c98ce8016dd4
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-25 14:34:02 +0200

    Update comments.c

--- comments.c
+++ comments.c
@@ -1,5 +1,14 @@
 #include "shell.h"
 
+/**
+ * remove_comments - Removes comments from the command string
+ * @command: The command string possibly containing comments
+ *
+ * Task 15: Comments
+ *
+ * Description: This function will remove anything after `#` in a command.
+ */
+
 void remove_comments(char *command)
 {
     char *comment_start = strchr(command, '#');
