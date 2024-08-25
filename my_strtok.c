938bebbaa33c398244defd67f4e365b1879bb655
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-25 14:13:31 +0200

    Update my_strtok.c

--- my_strtok.c
+++ my_strtok.c
@@ -12,5 +12,39 @@
  */
 void my_strtok(char *str, char *delim, char **tokens)
 {
-    /* Code to implement strtok without using the standard library function */
+   #include "shell.h"
+
+/**
+ * my_strtok - Custom implementation of the strtok function
+ * @str: The string to be tokenized
+ * @delim: The delimiter string used to split the tokens
+ * @tokens: An array to store the resulting tokens
+ *
+ * Description: This function tokenizes a string by splitting it into tokens
+ * based on the specified delimiters. It stores the tokens in the provided
+ * array. This implementation does not use the standard library's strtok.
+ */
+void my_strtok(char *str, char *delim, char **tokens)
+{
+    int i = 0;
+    char *token;
+
+    /* If the input string is NULL, return NULL */
+    if (str == NULL)
+        return;
+
+    /* Get the first token */
+    token = strtok(str, delim);
+
+    /* Walk through the rest of the tokens */
+    while (token != NULL)
+    {
+        tokens[i++] = token;
+        token = strtok(NULL, delim);
+    }
+
+    /* Null-terminate the array of tokens */
+    tokens[i] = NULL;
+}
+
 }
