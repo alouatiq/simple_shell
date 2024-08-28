f4a34a6f4bb998abf52949fe81231d5a7390f6a2
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-28 21:30:48 +0200

    NEW Update execute_command.c

--- execute_command.c
+++ execute_command.c
@@ -1,49 +1,46 @@
 #include "shell.h"
+#include <stdio.h>
+
+int execute_external_command(char **args);
 
 /**
- * contains_seperator - Checks if a command contains a seperator
+ * contains_separator - Checks if a command contains a separator
  * @command: String to be checked
- * @sep: The seperator to be checked (should be null terminated
+ * @sep: The separator to be checked (should be null terminated)
  *
- * Return: 0 if the command has seperators and -1 otherwise
+ * Return: 0 if the command has separators and -1 otherwise
  */
-int contains_seperator(char *command, char *sep)
+int contains_separator(char *command, char *sep)
 {
 	int i, j;
 	char *str = command;
 
-	i = 0;
-
 	if (command == NULL || sep == NULL)
 	{
-		perror("command_seperator not given enough arguments");
+		perror("command_separator not given enough arguments");
 		return (-1);
 	}
-	while (str[i] != '\0')
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] == sep[0])
 		{
-			j = 0;
-			while (sep[j] != '\0' && str[i + j] == sep[j])
-				j++;
-
+			for (j = 0; sep[j] != '\0' && str[i + j] == sep[j]; j++)
+				;
 			if (sep[j] == '\0')
-			{
 				return (0);
-			}
 		}
-		i++;
 	}
 
 	return (-1);
 }
 
 /**
- * built_ins - Function to handle built_ins in _execute command
+ * built_ins - Function to handle built-ins in _execute command
  * @args: An array of arguments
  * @i: Number of arguments
  *
- * Return: 0 if sucessful and -1 if failure;
+ * Return: 0 if successful, -1 if failure, and 2 for exit
  */
 int built_ins(char *args[], int i)
 {
@@ -52,35 +49,50 @@ int built_ins(char *args[], int i)
 		print_env();
 		return (0);
 	}
-	else if (_strcmp(args[0], "setenv") == 0 && i == 3)
+	if (_strcmp(args[0], "setenv") == 0 && i == 3)
+		return (my_setenv(args[1], args[2], 1));
+	if (_strcmp(args[0], "unsetenv") == 0 && i == 2)
+		return (my_unsetenv(args[1]));
+	if (_strcmp(args[0], "cd") == 0)
 	{
-		my_setenv(args[1], args[2], 1);
-		return (0);
+		if (i > 2)
+		{
+			fprintf(stderr, "cd: too many arguments\n");
+			return (-1);
+		}
+		change_directory(i == 1 ? NULL : args[1]);
+		return (0);  /* Assume success, change if needed */
 	}
-	else if (_strcmp(args[0], "unsetenv") == 0 && i == 2)
+	if (_strcmp(args[0], "exit") == 0)
 	{
-		my_unsetenv(args[1]);
-		return (0);
-	}
-	else if (_strcmp(args[0], "cd") == 0)
-	{
-		change_directory(args[1]);
-		return (0);
+		if (i > 2)
+		{
+			fprintf(stderr, "exit: too many arguments\n");
+			return (-1);
+		}
+		if (i == 2)
+		{
+			int status = atoi(args[1]);
+
+			exit(status);
+		}
+		return (2);  /* Signal to main loop to exit */
 	}
 	return (-1);
 }
+
 /**
- * tokenise - function that tockenises string
+ * tokenise - function that tokenises string
  * @command: String to be tokenised
  * @arg_count: Int to store number of arguments
  *
- * Return: Tockenised string
+ * Return: Tokenised string
  */
 char **tokenise(char *command, int *arg_count)
 {
 	char **args = malloc(100 * sizeof(char *));
 	char *token;
-	int i = 0;
+	int i = 0, j;
 
 	if (args == NULL)
 	{
@@ -91,7 +103,16 @@ char **tokenise(char *command, int *arg_count)
 	token = _strtok(command, " \n");
 	while (token != NULL && i < 99)
 	{
-		args[i++] = token;
+		args[i] = strdup(token);
+		if (args[i] == NULL)
+		{
+			perror("strdup failed");
+			for (j = 0; j < i; j++)
+				free(args[j]);
+			free(args);
+			return (NULL);
+		}
+		i++;
 		token = _strtok(NULL, " \n");
 	}
 	args[i] = NULL; /* Null-terminate the arguments array */
@@ -107,69 +128,99 @@ char **tokenise(char *command, int *arg_count)
  */
 int _executing(char **args)
 {
-    int status;
-    pid_t pid;
+	int status;
+	pid_t pid;
 
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork failed");
-        free(args);
-        exit(EXIT_FAILURE);
-    }
-    else if (pid == 0)
-    {
-        extern char **environ;
-
-        if (execve(args[0], args, environ) == -1)
-        {
-            /* Print error in the required format */
-            dprintf(STDERR_FILENO, "./hsh: 1: %s: not found\n", args[0]);
-            free(args);
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        do {
-            waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
-    free(args);
-    return (0);
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork failed");
+		return (-1);
+	}
+	else if (pid == 0)
+	{
+		if (execve(args[0], args, NULL) == -1)
+		{
+			/* Print error in the required format */
+			dprintf(STDERR_FILENO, "./hsh: 1: %s: not found\n", args[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return (0);
 }
 
 /**
  * execute_command - Parses and executes a command
  * @command: The command string to be executed
  *
- * Task 2: Simple Shell 0.2
+ * Description: This function handles the parsing of the command and
+ * its arguments, and delegates execution to appropriate functions.
  *
- * Description: This function will fork a new process and use execve
- * to run the command. It handles the parsing of the command and
- * its arguments, assuming the command is either an absolute path
- * or in the current directory.
- *
- * Return: 0 on success, or -1 on failure
+ * Return: 0 on success, -1 on failure, or 2 to signal shell exit
  */
 int execute_command(char *command)
 {
-	char **args; /* Array to hold arguments */
-	int i = 0;
+	char **args = NULL;
+	int i = 0, ret_val = 0;
+	int built_in_result;
 
-	if (contains_seperator(command, ";") == 0)
+	if (contains_separator(command, ";") == 0)
 	{
 		handle_command_separator(command);
 		return (0);
 	}
+
 	args = tokenise(command, &i);
 	if (args == NULL)
 		return (-1);
-	if (built_ins(args, i) == 0)
+
+	built_in_result = built_ins(args, i);
+	if (built_in_result == 0 || built_in_result == 2)
 	{
-		free(args);
-		return (0);
+		ret_val = built_in_result;
 	}
-	 _executing(args);
-	return (0);
+	else
+	{
+		ret_val = execute_external_command(args);
+	}
+
+	free_args(args);
+	return (ret_val);
+}
+
+/**
+ * execute_external_command - Executes an external command
+ * @args: Array of command arguments
+ *
+ * Description: This function resolves the command path using PATH
+ * and executes the command using fork and execve.
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int execute_external_command(char **args)
+{
+	char *full_path = NULL;
+	int ret_val = 0;
+	char *temp;
+
+	full_path = find_command_in_path(args[0]);
+	if (full_path == NULL)
+	{
+		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+		return (-1);
+	}
+
+	temp = args[0];
+	args[0] = full_path;
+	ret_val = _executing(args);
+	args[0] = temp;  /* Restore original args[0] */
+
+	free(full_path);
+	return (ret_val);
 }
