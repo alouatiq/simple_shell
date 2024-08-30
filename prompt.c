33f7ef13aabf5cddd244cf5f99a8873bdcbdaf7c
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-30 16:40:43 +0200

    Update prompt.c

--- prompt.c
+++ prompt.c
@@ -1,12 +1,6 @@
 #include "shell.h"
 
-/**
- * display_prompt - Displays the shell prompt.
- *
- * This function prints the shell prompt to the standard output,
- * indicating that the shell is ready to accept a command.
- */
 void display_prompt(void)
 {
-	write(STDOUT_FILENO, "($) ", 4);
+    write(STDOUT_FILENO, "($) ", 4);
 }
