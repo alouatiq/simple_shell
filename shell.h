b1277ed054c983b94f732d036a51a12655543e26
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-30 16:40:56 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -16,7 +16,7 @@ extern char **environ;
 
 /* Function prototypes */
 void display_prompt(void);
-void execute_command(char **args);
+int execute_command(char **args);
 ssize_t my_getline(char **lineptr, size_t *n, FILE *stream);
 char **custom_tokenize(char *line);
 void handle_exit(char **args);
@@ -25,12 +25,8 @@ int handle_unsetenv(const char *name);
 int handle_cd(char **args);
 int handle_command_separators(char *line);
 void handle_logical_operators(char *line);
-int handle_alias(char **args);
-char *handle_variable_replacement(char *line, int last_exit_status);
-char *remove_comments(char *line);
-int handle_file_input(const char *filename);
-char *find_command_in_path(char *command);
-int _setenv(const char *name, const char *value, int overwrite); /* Add this */
-int _unsetenv(const char *name);                                 /* Add this */
+int process_command(char *command);
+int _setenv(const char *name, const char *value, int overwrite);
+int _unsetenv(const char *name);
 
 #endif /* SHELL_H */
