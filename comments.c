d63b3a276c88eb675d07f2b82b02155e215c73b5
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-30 16:44:06 +0200

    Update comments.c

--- comments.c
+++ comments.c
@@ -1,22 +1,12 @@
 #include "shell.h"
 
-/**
- * remove_comments - Removes comments from the input line.
- * @line: The input line.
- *
- * Task 1.0: Handles comments `#`.
- *
- * Return: A pointer to the modified input line.
- */
 char *remove_comments(char *line)
 {
-	char *comment_start;
+    char *new_line = strdup(line);
+    char *hash_pos = strchr(new_line, '#');
 
-	comment_start = strchr(line, '#');
-	if (comment_start != NULL)
-	{
-	    *comment_start = '\0';  /* Truncate the line at the start of the comment */
-	}
+    if (hash_pos != NULL)
+        *hash_pos = '\0';
 
-	return (line);
+    return new_line;
 }
