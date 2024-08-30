f69a09bcf5d53f154f4dd750001678b8ce7dd067
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-30 16:43:46 +0200

    Update variables.c

--- variables.c
+++ variables.c
@@ -1,39 +1,31 @@
 #include "shell.h"
 
-/**
- * handle_variable_replacement - Handles variable replacement in the command line.
- * @line: The input line containing the command.
- * @last_exit_status: The status of the last executed command.
- *
- * Task 1.0: Handles variable replacement (`$?`, `$$`, etc.).
- *
- * Return: A new string with variables replaced, or NULL on failure.
- */
+/* Handles variable replacement ($?, $$, etc.) */
 char *handle_variable_replacement(char *line, int last_exit_status)
 {
-	char *new_line, *var_start;
-	char buffer[32];
-	int pid = getpid();
+    char *new_line = malloc(1024);
+    if (new_line == NULL)
+        return NULL;
 
-	new_line = strdup(line);
-	if (new_line == NULL)
-	    return (NULL);
+    char *ptr = new_line;
+    while (*line)
+    {
+        if (*line == '$' && *(line + 1) == '?')
+        {
+            ptr += sprintf(ptr, "%d", last_exit_status);
+            line += 2;
+        }
+        else if (*line == '$' && *(line + 1) == '$')
+        {
+            ptr += sprintf(ptr, "%d", getpid());
+            line += 2;
+        }
+        else
+        {
+            *ptr++ = *line++;
+        }
+    }
+    *ptr = '\0';
 
-	var_start = strstr(new_line, "$?");
-	if (var_start)
-	{
-	    snprintf(buffer, sizeof(buffer), "%d", last_exit_status);
-	    memmove(var_start + strlen(buffer), var_start + 2, strlen(var_start + 2) + 1);
-	    memcpy(var_start, buffer, strlen(buffer));
-	}
-
-	var_start = strstr(new_line, "$$");
-	if (var_start)
-	{
-	    snprintf(buffer, sizeof(buffer), "%d", pid);
-	    memmove(var_start + strlen(buffer), var_start + 2, strlen(var_start + 2) + 1);
-	    memcpy(var_start, buffer, strlen(buffer));
-	}
-
-	return (new_line);
+    return new_line;
 }
