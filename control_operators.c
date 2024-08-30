e6c78e18ba6d902e67fcce762cc58c1e1c98b639
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-30 16:43:11 +0200

    Update control_operators.c

--- control_operators.c
+++ control_operators.c
@@ -1,42 +1,39 @@
 #include "shell.h"
 
-/**
- * handle_command_separators - Handles command separators `;`.
- * @line: The input line containing the commands.
- *
- * Task 1.0: Handles command separators `;`.
- *
- * Return: 0 on success, -1 on failure.
- */
-
-int process_command(char *command);
-
-int handle_command_separators(char *line) {
+int handle_command_separators(char *line)
+{
     char *command;
     int status = 0;
 
     command = strtok(line, ";");
-    while (command != NULL) {
-        status = process_command(command);  /* Call to process individual commands */
+    while (command != NULL)
+    {
+        status = process_command(command);
         command = strtok(NULL, ";");
     }
 
     return status;
 }
 
-void handle_logical_operators(char *line) {
+void handle_logical_operators(char *line)
+{
     char *command;
     int status = 0;
-    int execute_next = 1;  /* Flag to indicate whether the next command should be executed */
+    int execute_next = 1;
 
     command = strtok(line, "&&||");
-    while (command != NULL) {
-        if (execute_next) {
-            status = process_command(command);  /* Process the command */
-            if (strstr(line, "&&") != NULL) {
-                execute_next = (status == 0);  /* Continue only if last command was successful */
-            } else if (strstr(line, "||") != NULL) {
-                execute_next = (status != 0);  /* Continue only if last command failed */
+    while (command != NULL)
+    {
+        if (execute_next)
+        {
+            status = process_command(command);
+            if (strstr(line, "&&") != NULL)
+            {
+                execute_next = (status == 0);
+            }
+            else if (strstr(line, "||") != NULL)
+            {
+                execute_next = (status != 0);
             }
         }
         command = strtok(NULL, "&&||");
