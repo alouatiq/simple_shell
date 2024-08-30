14136c6b76dbb039b1ceb96598240327f01a27a6
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-08-30 16:42:19 +0200

    Update cd.c

--- cd.c
+++ cd.c
@@ -1,52 +1,49 @@
 #include "shell.h"
-#include <stdlib.h>  /* Required for custom _setenv */
-#include <string.h>  /* Required for strlen, strcpy */
 
-/**
- * handle_cd - Handles the built-in `cd` command.
- * @args: An array of arguments.
- *
- * Task 1.0: Handles `cd`.
- *
- * Return: 0 on success, -1 on failure.
- */
-
-extern char **environ;
-
-int handle_cd(char **args) {
+int handle_cd(char **args)
+{
     char *dir;
     char cwd[1024];
 
-    if (args[1] == NULL) {  /* No argument provided, go to HOME */
+    if (args[1] == NULL)
+    {
         dir = getenv("HOME");
-        if (dir == NULL) {
+        if (dir == NULL)
+        {
             perror("cd");
             return (-1);
         }
-    } else if (strcmp(args[1], "-") == 0) {  /* Change to previous directory */
+    }
+    else if (strcmp(args[1], "-") == 0)
+    {
         dir = getenv("OLDPWD");
-        if (dir == NULL) {
+        if (dir == NULL)
+        {
             perror("cd");
             return (-1);
         }
         printf("%s\n", dir);
-    } else {
+    }
+    else
+    {
         dir = args[1];
     }
 
-    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+    if (getcwd(cwd, sizeof(cwd)) == NULL)
+    {
         perror("getcwd");
         return (-1);
     }
 
-    if (chdir(dir) != 0) {  /* Change directory */
+    if (chdir(dir) != 0)
+    {
         perror("cd");
         return (-1);
     }
 
-    /* Update environment variables */
     _setenv("OLDPWD", cwd, 1);
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
         _setenv("PWD", cwd, 1);
     }
 
