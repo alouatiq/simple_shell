1d570d9acbfc08756ac29314c573d772510fed8e
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-10-19 13:18:57 +0200

    Create str_functions.c

---
+++ str_functions.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * _strcmp - Compare two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: 0 if the strings are equal, otherwise a non-zero value
+ */
+int _strcmp(const char *s1, const char *s2)
+{
+    while (*s1 && *s2 && *s1 == *s2)
+    {
+        s1++;
+        s2++;
+    }
+    return (*s1 - *s2);
+}
