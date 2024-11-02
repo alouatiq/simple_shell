7345ef5afded8c29cac0081977e06e67c4c537ac
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-02 10:36:44 +0100

    Create resolve_path.c

---
+++ resolve_path.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+char *resolve_path(char *command)
+{
+    char *path = getenv("PATH"), *token, *full_path;
+    struct stat st;
+
+    if (!path)
+        return NULL;
+    if (stat(command, &st) == 0)
+        return strdup(command);
+
+    token = strtok(path, ":");
+    while (token)
+    {
+        full_path = malloc(strlen(token) + strlen(command) + 2);
+        sprintf(full_path, "%s/%s", token, command);
+        
+        if (stat(full_path, &st) == 0)
+            return full_path;
+
+        free(full_path);
+        token = strtok(NULL, ":");
+    }
+    return NULL;
+}
