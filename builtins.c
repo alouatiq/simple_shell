7a99e7e523c98b506c201953e2bace93512ffd94
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-02 10:56:39 +0100

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -4,19 +4,19 @@
  * shell_exit - Built-in command to exit the shell.
  * @args: Argument list (not used in this function).
  *
- * Return: 0 to signal exit in the main loop.
+ * Return: (0) to signal exit in the main loop.
  */
 int shell_exit(char **args)
 {
     (void)args; /* Suppress unused parameter warning */
-    return 0; /* Return 0 to signal exit in main loop */
+    return (0);
 }
 
 /**
  * shell_env - Built-in command to print environment variables.
  * @args: Argument list (not used in this function).
  *
- * Return: 1 to continue the shell loop.
+ * Return: (1) to continue the shell loop.
  */
 int shell_env(char **args)
 {
@@ -29,5 +29,21 @@ int shell_env(char **args)
         printf("%s\n", *env);
         env++;
     }
-    return 1;
+    return (1);
+}
+
+/**
+ * check_builtin - Checks if a command is a built-in command and executes it.
+ * @args: Argument list for the command.
+ *
+ * Return: (1) if the command is a built-in, (0) otherwise.
+ */
+int check_builtin(char **args)
+{
+    if (strcmp(args[0], "exit") == 0)
+        return (shell_exit(args));
+    else if (strcmp(args[0], "env") == 0)
+        return (shell_env(args));
+    
+    return (0);
 }
