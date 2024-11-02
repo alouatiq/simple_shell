a2d5ca4959bb3ac8cbc337bd12a5e0f49efd5a6f
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-02 10:38:06 +0100

    Create memory_helpers.c

---
+++ memory_helpers.c
@@ -0,0 +1,67 @@
+#include "shell.h"
+
+/**
+ * _realloc - Reallocates a memory block to a new size.
+ * @ptr: Pointer to the previous memory block.
+ * @old_size: The size of the old memory block.
+ * @new_size: The size of the new memory block.
+ *
+ * Return: Pointer to the newly allocated memory block, or NULL on failure.
+ */
+void *_realloc(void *ptr, size_t old_size, size_t new_size)
+{
+    void *new_ptr;
+    size_t min_size;
+
+    if (new_size == old_size)
+        return ptr;
+    if (new_size == 0 && ptr)
+    {
+        free(ptr);
+        return NULL;
+    }
+    if (!ptr)
+        return malloc(new_size);
+
+    new_ptr = malloc(new_size);
+    if (!new_ptr)
+        return NULL;
+
+    min_size = old_size < new_size ? old_size : new_size;
+    memcpy(new_ptr, ptr, min_size);
+    free(ptr);
+    return new_ptr;
+}
+
+/**
+ * free_tokens - Frees a NULL-terminated array of strings.
+ * @tokens: Array of strings to be freed.
+ */
+void free_tokens(char **tokens)
+{
+    int i;
+
+    if (tokens)
+    {
+        for (i = 0; tokens[i]; i++)
+            free(tokens[i]);
+        free(tokens);
+    }
+}
+
+/**
+ * _memset - Fills a block of memory with a constant byte.
+ * @s: Pointer to the memory block.
+ * @c: The byte to fill the memory with.
+ * @n: The number of bytes to fill.
+ *
+ * Return: Pointer to the memory block.
+ */
+void *_memset(void *s, int c, size_t n)
+{
+    unsigned char *ptr = s;
+
+    while (n--)
+        *ptr++ = (unsigned char)c;
+    return s;
+}
