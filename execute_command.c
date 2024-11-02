34d683b199cdc707cf22a35272a1ee2b60dd0ea4
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-02 10:36:35 +0100

    Create execute_command.c

---
+++ execute_command.c
@@ -0,0 +1,34 @@
+#include "shell.h"
+
+int execute_command(char **args)
+{
+    pid_t pid;
+    int status;
+    char *command_path;
+
+    command_path = resolve_path(args[0]);
+    if (!command_path)
+    {
+        fprintf(stderr, "%s: command not found\n", args[0]);
+        return 1;
+    }
+
+    pid = fork();
+    if (pid == 0)
+    {
+        if (execve(command_path, args, environ) == -1)
+            perror("execve");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid < 0)
+    {
+        perror("fork");
+    }
+    else
+    {
+        waitpid(pid, &status, 0);
+    }
+
+    free(command_path);
+    return 1;
+}
