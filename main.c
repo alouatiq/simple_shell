26581cf77fb82924b1a11c036e8cff4e97508ddd
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2024-11-02 10:36:03 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+int main(void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read_len;
+    int status = 1;
+
+    while (status)
+    {
+        if (isatty(STDIN_FILENO))
+            printf("$ ");
+        
+        read_len = getline(&line, &len, stdin);
+        if (read_len == -1)
+        {
+            free(line);
+            break;
+        }
+        
+        line[read_len - 1] = '\0'; // Remove newline character
+        status = execute_line(line);
+    }
+    
+    free(line);
+    return 0;
+}
